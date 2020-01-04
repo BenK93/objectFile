633ee8ac21c0dfbe0c57b7fed8457ec2966871de
BenK93 <benkoren23@gmail.com>
2020-01-04 11:36:00 +0200

    third commit

--- main.c
+++ main.c
@@ -16,13 +16,17 @@ int numOfVertexs(FILE *file);
 
 int numOfFaces(FILE *file);
 
-void initVertex(Vertex *vertexes, int quantity,FILE *file);
+Scene *createScene(char *fileName, ...);
 
-void initFaces(Face *faces, int quantity, FILE *file);
+void initVertex(Vertex *vertexes,FILE *file);
+
+void initFaces(Face *faces, FILE *file);
+
+int getLen(char *line);
 
 int main() {
     transformObject("Donald.obj","DeformedDonald.obj");
-//    Object *obj = createObject("Donald.obj");
+    Object *obj = createObject("Donald.obj");
     return 0;
 }
 
@@ -58,41 +62,85 @@ void convertToXYZ(char *line, FILE *pFile) {
 }
 Object *createObject (char *filename) {
     FILE *f = fopen(filename, "r");
+    FILE *f2 = fopen(filename, "r");
     if (f == NULL){
-        printf("Error thers is problem with the file.");
+        printf("Error there is a problem with the file.");
         return NULL;
     }
     int vQuantity = numOfVertexs(f);
-    int fQuantity = numOfFaces(f);
+    int fQuantity = numOfFaces(f2);
     Object *obj = (Object*)malloc(sizeof(Object));
     obj->numberOfVertexes = vQuantity;
     obj->numberOfFaces = fQuantity;
     obj->faces = (Face*)malloc(fQuantity*sizeof(Face));
     obj->vertexes = (Vertex*)malloc(vQuantity*sizeof(Vertex));
-    initVertex(obj->vertexes,vQuantity,f);
-    initFaces(obj->faces,fQuantity,f);
+    rewind(f);
+    rewind(f2);
+    initVertex(obj->vertexes,f);
+    initFaces(obj->faces,f2);
+
     return obj;
 }
 
-void initFaces(Face *faces, int quantity, FILE *file) {
-    int *arrVer = faces->vertex;
-    arrVer = (int *)malloc(quantity* sizeof(int));
+void initFaces(Face *faces,FILE *file) {
     char *line =(char *)malloc(sizeof(char));
     size_t len =0;
-    int num;
+    int num = 0;
     int i =0;
+    int indexVer = 0;
+    int faceindex = 0;
+    int length = 0;
+    int flag = 0;
     while (!feof(file)) {
         getline(&line, &len, file);
         if(line[0] == 'f' && line[1] == ' '){
-//            sscanf(line,"%*[^0123456789]%d%*[^0123456789]%d%*[^0123456789]%d", &x,&y,&z);
-            char *token =
-            i++;
+            length = getLen(line);
+            faces[faceindex].vertex = (int *)malloc( length*sizeof(int));
+            char *tempNum = (char*)malloc(length* sizeof(char));
+            for (int j = 0; j < strlen(line); ++j) {
+                if(line[j] == ' '){
+                    flag = 1;
+                }else if(line[j]== '/'){
+                    flag = 0;
+                    i = 0;
+                    sscanf(tempNum,"%d",&num);
+                }else if(flag == 1){
+                    tempNum[i] = line[j];
+                    i++;
+                }
+                if(tempNum[0] != '\0' && flag == 0){
+                    faces[faceindex].vertex[indexVer] = num;
+                    indexVer++;
+                    tempNum[0] = '\0';
+                }
+            }
+            free(tempNum);
+            faces->size = length;
+            indexVer=0;
+            faceindex++;
         }
     }
     free(line);
 }
 
-void initVertex(Vertex *vertexes, int quantity,FILE *file) {
+int getLen(char *line) {
+    char *token = (char *)malloc(4*sizeof(char));
+    int flag = 0;int len = 0;int index = 0;
+    for (int i = 0; i < 8; ++i) {
+        if(line[i] == ' '){
+            flag = 1;
+        }else if(line[i]== '/'){
+            len = strlen(token);
+            break;
+        }else if(flag ==1){
+            token[index] = line[i];
+            index++;
+        }
+    }
+    return len;
+}
+
+void initVertex(Vertex *vertexes,FILE *file) {
     char *line =(char *)malloc(sizeof(char));
     size_t len =0;
     float x,y,z;
